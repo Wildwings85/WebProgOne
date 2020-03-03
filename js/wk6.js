af5718099593beaf7f4977e277f31c9bdb1d9d59
Wildwings85 <59941702+Wildwings85@users.noreply.github.com>
2020-03-03 14:29:01 -0500

    1

---
+++ js/wk6.js
@@ -0,0 +1,62 @@
+var s1 = {
+    title: "Interpretive Listening",
+    description: "The student will be able to understand and interpret information, concepts, and ideas orally from culturally authentic sources on a variety of topics in the target language.",
+    NM1: "Demonstrate understanding of basic words, phrases, and questions about self and personal experiences through gestures, drawings, pictures, and actions.",
+    NM2: "Demonstrate understanding of everyday expressions dealing with simple and concrete daily activities and needs presented in a clear, slow, and repeated speech.",
+    NM3: "Demonstrate understanding of basic words and phrases in simple messages and announcements in familiar settings.",
+    NM4: "Demonstrate understanding of simple information supported by visuals through a variety of media.",
+    NM5: "Demonstrate understanding of simple rhymes, songs, poems, and read aloud stories.",
+    NM6: "Follow short, simple directions.",
+};
+var se1 = "<b>Title: </b>" + s1.title + "<br> <b>Description: </b>" + s1.description + "<br> <b>WL.K12.NM.1.1: </b>" + s1.NM1 + "<br> <b>WL.K12.NM.1.2: </b>" + s1.NM2 + "<br> <b>WL.K12.NM.1.3: </b>" + s1.NM3 + "<br> <b>WL.K12.NM.1.4: </b>" + s1.NM4 + "<br> <b>WL.K12.NM.1.5: </b>" + s1.NM5 + "<br> <b>WL.K12.NM.1.6: </b>" + s1.NM6 +""
+
+var s2 = {
+    title: "Interpretative Reading",
+    description: "The student will be able to understand and interpret information, concepts, and ideas in writing from culturally authentic sources on a variety of topics in the target language.",
+    NM1: "Demonstrate understanding of written familiar words, phrases, and simple sentences supported by visuals.",
+    NM2: "Demonstrate understanding of short, simple literary stories.",
+    NM3: "Demonstrate understanding of simple written announcements with prompting and support.",
+    NM4: "Recognize words and phrases when used in context on familiar topics.",
+};
+var se2 = "<b>Title: </b>" + s2.title + "<br> <b>Description: </b>" + s2.description + "<br> <b>WL.K12.NM.2.1: </b>" + s2.NM1 + "<br> <b>WL.K12.NM.2.2: </b>" + s2.NM2 + "<br> <b>WL.K12.NM.2.3: </b>" + s2.NM3 + "<br> <b>WL.K12.NM.2.4: </b>" + s2.NM4 + ""
+
+var s3 = {
+    title: "Interpersonal Communication",
+    description: "The student will be able to engage in conversations and exchange information, concepts, and ideas orally and in writing with a variety of speakers or readers on a variety of topics in a culturally appropriate context in the target language.",
+    NM1: "Introduce self and others using basic, culturally appropriate greetings.",
+    NM2: "Participate in basic conversations using words, phrases, and memorized expressions.",
+    NM3: "Ask simple questions and provide simple responses related to personal preferences.",
+    NM4: "Exchange essential information about self, family, and familiar topics.",
+    NM5: "Understand and use in context common concepts (such as numbers, days of the week, etc.) in simple situations.",
+    NM6: "Use appropriate gestures, body language, and intonation to clarify a message",
+    NM7: "Understand and respond appropriately to simple directions.",
+    NM8: "Differentiate among oral statements, questions, and exclamations in order to determine meaning.",
+};
+var se3 = "<b>Title: </b>" + s3.title + "<br> <b>Description: </b>" + s3.description + "<br> <b>WL.K12.NM.3.1: </b>" + s3.NM1 + "<br> <b>WL.K12.NM.3.2: </b>" + s3.NM2 + "<br> <b>WL.K12.NM.3.3: </b>" + s3.NM3 + "<br> <b>WL.K12.NM.3.4: </b>" + s3.NM4 + "<br> <b>WL.K12.NM.3.5: </b>" + s3.NM5 + "<br> <b>WL.K12.NM.3.6: </b>" + s3.NM6 +"<br> <b>WL.K12.NM.3.7: </b>" + s3.NM7 +"<br> <b>WL.K12.NM.3.8: </b>" + s3.NM8 +""
+
+var s4 = {
+    title: "Presentational Speaking",
+    description: " The student will be able to present information, concepts, and ideas to an audience of listeners on a variety of topics in a culturally appropriate context in the target language.",
+    NM1: "Provide basic information about self and immediate surroundings using words and phrases and memorized expressions.",
+    NM2: "Present personal information about self and others.",
+    NM3: "Express likes and dislikes.",
+    NM4: "Provide an account of daily activities.",
+    NM5: "Role-play skits, songs, or poetry in the target language that deal with familiar topics",
+    NM6: "Present simple information about a familiar topic using visuals.",
+};
+var se4 = "<b>Title: </b>" + s4.title + "<br> <b>Description: </b>" + s4.description + "<br> <b>WL.K12.NM.4.1: </b>" + s4.NM1 + "<br> <b>WL.K12.NM.4.2: </b>" + s4.NM2 + "<br> <b>WL.K12.NM.4.3: </b>" + s4.NM3 + "<br> <b>WL.K12.NM.4.4: </b>" + s4.NM4 + "<br> <b>WL.K12.NM.4.5: </b>" + s4.NM5 + "<br> <b>WL.K12.NM.4.6: </b>" + s4.NM6 +""
+
+var s5 = {
+    title: "Presentational Writing",
+    description: "The student will be able to present information, concepts, and ideas to an audience of readers on a variety of topics in a culturally appropriate context in the target language.",
+    NM1: "Provide basic information in writing using familiar topics, often using previously learned expressions and phrases.",
+    NM2: "Fill out a simple form with basic information.",
+    NM3: "Write simple sentences about self and/or others",
+    NM4: "Write simple sentences that help in day-to-day life communication.",
+    NM5: "Write about previously acquired knowledge and experiences.",
+    NM6: "Pre-write by drawing pictures to support ideas related to a task.",
+    NM7: "Draw pictures in sequence to demonstrate a story plot.",
+};
+var se5 = "<b>Title: </b>" + s5.title + "<br> <b>Description: </b>" + s5.description + "<br> <b>WL.K12.NM.5.1: </b>" + s5.NM1 + "<br> <b>WL.K12.NM.5.2: </b>" + s5.NM2 + "<br> <b>WL.K12.NM.5.3: </b>" + s5.NM3 + "<br> <b>WL.K12.NM.5.4: </b>" + s5.NM4 + "<br> <b>WL.K12.NM.5.5: </b>" + s5.NM5 + "<br> <b>WL.K12.NM.5.6: </b>" + s5.NM6 +"<br> <b>WL.K12.NM.5.7: </b>" + s5.NM7 +""
+
+
