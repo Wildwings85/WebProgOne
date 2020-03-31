47d414c2bec9a396cae9b2433fb3a35cf4730c5a
Wildwings85 <59941702+Wildwings85@users.noreply.github.com>
2020-03-31 16:55:39 -0400

    1

--- js/wk9.js
+++ js/wk9.js
@@ -80,6 +80,14 @@ function myFunction2() {
         x.style.display = "block";
     };
 }
+function myFunction4() {
+    var x = document.getElementById("output2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    };
+}
 //loop 
 function myFunction3() {
     var five = ["HANDS - Wash them often", "ELBOW - Cough into it", "FACE - Don't touch it", "SPACE - Keep safe distance", "HOME - Stay if you can"];
@@ -89,4 +97,22 @@ function myFunction3() {
         text += five[i] + "<br>";
     }
     document.getElementById("demo").innerHTML = text;
+}
+//random
+var quotes = [
+	"It is only in our darkest hours that we may discover the true strength of the brilliant light within ourselves that can never, ever, be dimmed. – Doe Zantamata",
+    "Hope is important because it can make the present moment less difficult to bear. If we believe that tomorrow will be better, we can bear a hardship today. – Thich Nhat Hanh",
+    "I will love the light for it shows me the way, yet I will endure the darkness for it shows me the stars. – Og Mandino",
+    "Have great hopes and dare to go all out for them. Have great dreams and dare to live them. Have tremendous expectations and believe in them. – Norman Vincent Peale",
+    "The gem cannot be polished without friction, nor man perfected without trials. – Chinese Proverb",
+    "Every adversity, every failure and every heartache carries with it the seed of an equivalent or a greater benefit. – Napoleon Hill",
+    "I ask not for a lighter burden, but for broader shoulders. – Jewish Proverb",
+    "Tough times never last, but tough people do. – Robert H Schuller",
+    "Never give in. Never, never, never. – Winston Churchill",
+    "Prosperity is not without many fears and disasters; and adversity is not without comforts and hopes. – Francis Bacon",
+    "The greater the difficulty, the more glory in surmounting it. Skillful pilots gain their reputation from storms and tempests. – Epicetus",
+]
+function getRnd() {
+	var getRndInteger = Math.floor(Math.random() * (quotes.length));
+	document.getElementById('random').innerHTML = quotes[getRndInteger];
 }
