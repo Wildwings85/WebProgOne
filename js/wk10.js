ea6bcecee87e0a5f71ca4537cd679136f9de5374
Wildwings85 <59941702+Wildwings85@users.noreply.github.com>
2020-04-17 17:23:41 -0400

    1

---
+++ js/wk10.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+  document.getElementById("output").innerHTML = "<iframe src='https://youtu.be/7-B4-ZIRVMg'></iframe>";
+}
+function myFunction2() {
+    document.getElementById("output").innerHTML = "<iframe src='https://youtu.be/vNUHi2p3yGM'></iframe>";
+  }
+// Arrays
+var idv = [
+    "https://www.youtube.com/watch?v=7-B4-ZIRVMg",
+    "https://www.youtube.com/watch?v=vNUHi2p3yGM",
+    "https://www.youtube.com/watch?v=Z1sIlfSTYoM",
+    "https://www.youtube.com/watch?v=3qZ549IjeHo",
+    "https://www.youtube.com/watch?v=Kg0QqMz1dps",
+    "https://www.youtube.com/watch?v=ZbNKTOXUVow",
+    "https://www.youtube.com/watch?v=Z3jZtTtrRNU",
+    "https://www.youtube.com/watch?v=feMWLRRh7Cg",
+]
